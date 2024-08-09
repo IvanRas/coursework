9459934e668ac51f6c3fd3d83cee29b6d9bf6d31
root <rastorgu98@gmail.com>
2024-08-09 11:49:34 +0300

    функция work_time

--- src/views.py
+++ src/views.py
@@ -13,21 +13,22 @@ PATH_TO_FILE_EXCEL = os.path.join(os.path.dirname(os.path.dirname(__file__)), "d
 
 def work_time():
     date_string = datetime.datetime.now()
-    time_obj = datetime.datetime(2022, 3, 8, 15, 45, 0)
-    time_string = time_obj.strftime
+    date_str = date_string.strftime('%H:%M')
+    date = datetime.datetime.today()
+    time_string = date.strftime('%H:%M')
     new_time_morn = time_string + datetime.timedelta(hours=6, minutes=00)
     new_time_day = time_string + datetime.timedelta(hours=12, minutes=00)
     new_time_evening = time_string + datetime.timedelta(hours=18, minutes=00)
     new_time_nights = time_string + datetime.timedelta(hours=24, minutes=00)
 
     print(date_string)
-    if date_string < new_time_morn:
+    if date_str < new_time_morn:
         print("Доброе ночи")
-    elif new_time_morn < date_string < new_time_day:
+    elif new_time_morn < date_str < new_time_day:
         print("Добрый  утро")
-    elif new_time_day < date_string < new_time_evening:
+    elif new_time_day < date_str < new_time_evening:
         print("Добрый  день")
-    elif new_time_evening < date_string < new_time_nights:
+    elif new_time_evening < date_str < new_time_nights:
         print("Добрый  вечер")
     else:
         print("error")
@@ -38,8 +39,10 @@ def get_mask_card_number(card_number: str) -> str:
     return f"{card_number[:4]} {card_number[4:8]} **** "
 
 
-def total_amount_of_expenses():
-    """Функция подсчета общих расходов"""
+def total_amount_of_expenses() -> dict:
+    """Функция подсчета общих расходов."""
+    # из столбца operation_amount/Сумма операции взять те что идут с минусом и составить
+    # новый список включающий только расзоды
     total = 0
     transactions = get_transactions_dictionary_excel(PATH_TO_FILE_EXCEL)
     for i in transactions["operation_amount"]:
@@ -50,8 +53,8 @@ def total_amount_of_expenses():
     return total
 
 
-def total_category():
-    """Функция подсчета общих расходов"""
+def total_category(transactions: dict) -> dict:
+    """Функция подсчета кешбэка."""
     cash = 0
     transactions = get_transactions_dictionary_excel(PATH_TO_FILE_EXCEL)
     for i in transactions["cashback"]:
@@ -59,16 +62,19 @@ def total_category():
     return cash
 
 
-def top_five():
-    """Функция подсчета общих расходов"""
+def top_five(total: dict) -> dict:
+    """Функция топа 5 расзодов"""
     top = []
-    transactions = get_transactions_dictionary_excel(PATH_TO_FILE_EXCEL)
-    for i in transactions["cashback"]:
+    transactions =
+    # нужно достать из total_amount_of_expenses() total и отсортировать по убыванию
+    # после вывести первые 5
+    for i in
         top.append(i)[:5]
     return top
 
 
 def shares_sp500():
+    """Функция подсчета Стоимость акций из S&P500."""
     url = "https://query2.finance.yahoo.com/v8/finance/chart/%5EGSPC"
     headers = {"apikey": api_key}
 
