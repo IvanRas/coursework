59d15c9de35eb118a4379b60b69a930e30cdc3f2
root <rastorgu98@gmail.com>
2024-08-13 08:20:22 +0300

    курсовая редакция test_reports

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from unittest.mock import mock_open, patch
 
 import pandas as pd
@@ -8,20 +7,22 @@ from src.reports import spending_by_category
 
 
 def test_spending_by_category(samp_transactions: pd.DataFrame, test_operations) -> None:
-    df_transactions = pd.DataFrame(samp_transactions)
-    current_datetime = "20.04.2023 12:00:00"
-    category = "Еда"
+    @spending_by_category()
+    def func():
+        df_transactions = pd.DataFrame(samp_transactions)
+        current_datetime = "20.04.2023 12:00:00"
+        category = "Еда"
 
-    mock_open_func = mock_open()
+        mock_open_func = mock_open()
 
-    with patch("builtins.open", mock_open_func):
-        result = spending_by_category(df_transactions, category, current_datetime).to_dict(orient="records")
+        with patch("builtins.open", mock_open_func):
+            result = spending_by_category(df_transactions, category, current_datetime).to_dict(orient="records")
 
-    expected_result = [
-        {"Дата операции": "10.03.2023 12:00:00", "Категория": "Еда", "Сумма": 200},
-        {"Дата операции": "20.04.2023 12:00:00", "Категория": "Еда", "Сумма": 300},
-    ]
-    assert result == expected_result
+        expected_result = [
+            {"Дата операции": "10.03.2023 12:00:00", "Категория": "Еда", "Сумма": 200},
+            {"Дата операции": "20.04.2023 12:00:00", "Категория": "Еда", "Сумма": 300},
+        ]
+        assert result == expected_result
 
-    mock_open_func.assert_called_once_with("../logs/log_file.json", "w", encoding="utf-8")
+        mock_open_func.assert_called_once_with("../logs/log_file.json", "w", encoding="utf-8")
 
